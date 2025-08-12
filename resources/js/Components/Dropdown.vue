92e6877028627681bd097eeb57e0e05b9aa04b59
Zandri12 <zandri081@gmail.com>
2025-08-12 23:54:48 +0700

    Enhance Dropdown component with transition effects and click outside detection; update DropdownLink to handle post requests and emit click events.

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,19 +1,51 @@
 <template>
-  <div class="relative">
-    <div @click="open = !open">
+  <div class="relative" ref="dropdownRef">
+    <div @click="toggleDropdown">
       <slot name="trigger" />
     </div>
 
-    <div v-show="open" class="absolute z-50 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
-      <slot />
-    </div>
+    <transition
+      enter-active-class="transition ease-out duration-100"
+      enter-from-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100"
+      leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100"
+      leave-to-class="transform opacity-0 scale-95"
+    >
+      <div 
+        v-if="open" 
+        :class="[
+          'absolute z-50 mt-2 w-48 bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5',
+          align === 'right' ? 'right-0' : 'left-0'
+        ]"
+      >
+        <slot />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, nextTick } from 'vue'
+
+const props = defineProps({
+  align: {
+    type: String,
+    default: 'left'
+  },
+  width: {
+    type: String,
+    default: '48'
+  }
+})
 
 const open = ref(false)
+const dropdownRef = ref(null)
+
+const toggleDropdown = () => {
+  open.value = !open.value
+  console.log('Dropdown toggled:', open.value)
+}
 
 const closeOnEscape = (e) => {
   if (open.value && e.key === 'Escape') {
@@ -21,11 +53,25 @@ const closeOnEscape = (e) => {
   }
 }
 
+const closeOnClickOutside = (e) => {
+  if (open.value && dropdownRef.value && !dropdownRef.value.contains(e.target)) {
+    open.value = false
+  }
+}
+
+const closeDropdown = () => {
+  open.value = false
+}
+
 onMounted(() => {
   document.addEventListener('keydown', closeOnEscape)
+  document.addEventListener('click', closeOnClickOutside)
+  
+  console.log('Dropdown component mounted')
 })
 
 onUnmounted(() => {
   document.removeEventListener('keydown', closeOnEscape)
+  document.removeEventListener('click', closeOnClickOutside)
 })
 </script>
--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -2,20 +2,43 @@
   <a
     :href="href"
     :class="[
-      'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out',
+      'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out cursor-pointer',
       $attrs.class
     ]"
     v-bind="$attrs"
+    @click="handleClick"
   >
     <slot />
   </a>
 </template>
 
 <script setup>
-defineProps({
+import { useForm } from '@inertiajs/vue3'
+
+const props = defineProps({
   href: {
     type: String,
     required: true
+  },
+  method: {
+    type: String,
+    default: 'get'
+  },
+  as: {
+    type: String,
+    default: 'a'
   }
 })
+
+const emit = defineEmits(['click'])
+
+const handleClick = (e) => {
+  if (props.method === 'post') {
+    e.preventDefault()
+    const form = useForm({})
+    form.post(props.href)
+  }
+  
+  emit('click', e)
+}
 </script>
