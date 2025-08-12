a79aff8d687b7a92c2cb3b742c68241426b1f4a6
Zandri12 <zandri081@gmail.com>
2025-08-13 00:07:24 +0700

    Refactor Dropdown component to simplify structure and remove unused props; update AuthenticatedLayout to reflect changes in Dropdown usage.

--- resources/js/Components/Dropdown.vue
+++ resources/js/Components/Dropdown.vue
@@ -1,77 +1,28 @@
 <template>
-  <div class="relative" ref="dropdownRef">
-    <div @click="toggleDropdown">
-      <slot name="trigger" />
-    </div>
-
-    <transition
-      enter-active-class="transition ease-out duration-100"
-      enter-from-class="transform opacity-0 scale-95"
-      enter-to-class="transform opacity-100 scale-100"
-      leave-active-class="transition ease-in duration-75"
-      leave-from-class="transform opacity-100 scale-100"
-      leave-to-class="transform opacity-0 scale-95"
+  <div class="relative">
+    <button 
+      @click="toggleDropdown"
+      type="button"
+      class="inline-flex items-center rounded-md border border-transparent bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
     >
-      <div 
-        v-if="open" 
-        :class="[
-          'absolute z-50 mt-2 w-48 bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5',
-          align === 'right' ? 'right-0' : 'left-0'
-        ]"
-      >
-        <slot />
-      </div>
-    </transition>
+      <slot name="trigger" />
+    </button>
+
+    <div 
+      v-show="open" 
+      class="absolute right-0 z-50 mt-2 w-48 bg-white rounded-md shadow-lg py-1 ring-1 ring-black ring-opacity-5"
+    >
+      <slot />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, nextTick } from 'vue'
-
-const props = defineProps({
-  align: {
-    type: String,
-    default: 'left'
-  },
-  width: {
-    type: String,
-    default: '48'
-  }
-})
+import { ref } from 'vue'
 
 const open = ref(false)
-const dropdownRef = ref(null)
 
 const toggleDropdown = () => {
   open.value = !open.value
-  console.log('Dropdown toggled:', open.value)
 }
-
-const closeOnEscape = (e) => {
-  if (open.value && e.key === 'Escape') {
-    open.value = false
-  }
-}
-
-const closeOnClickOutside = (e) => {
-  if (open.value && dropdownRef.value && !dropdownRef.value.contains(e.target)) {
-    open.value = false
-  }
-}
-
-const closeDropdown = () => {
-  open.value = false
-}
-
-onMounted(() => {
-  document.addEventListener('keydown', closeOnEscape)
-  document.addEventListener('click', closeOnClickOutside)
-  
-  console.log('Dropdown component mounted')
-})
-
-onUnmounted(() => {
-  document.removeEventListener('keydown', closeOnEscape)
-  document.removeEventListener('click', closeOnClickOutside)
-})
 </script>
